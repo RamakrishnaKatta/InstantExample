fa06b7d41b2e4492262920b6a185ddafb8bb8339
ramky <ramky.lll@outlook.com>
2018-07-30 17:01:11 +0530

    Initial commit

---
+++ basefeature/src/main/java/ramky/com/instantexample/MainActivity.java
@@ -0,0 +1,21 @@
+package ramky.com.instantexample;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import ramky.com.instantexample.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // ATTENTION: This was auto-generated to handle app links.
+        Intent appLinkIntent = getIntent();
+        String appLinkAction = appLinkIntent.getAction();
+        Uri appLinkData = appLinkIntent.getData();
+    }
+}
